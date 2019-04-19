2adf3696c332b98f48dd6875d8b50e90362f98cd
Matthew Markfort <matthew.markfort@gmail.com>
2019-04-19 13:25:25 -0500

    Include catch for errors during parse_json call

--- F5/switch_domain.tcl
+++ F5/switch_domain.tcl
@@ -1,16 +1,21 @@
 when HTTP_REQUEST {
-    if { [HTTP::header exists "Content-Length"] && [HTTP::method] == "POST" }{
-        HTTP::collect [HTTP::header "Content-Length"]
+    if { [HTTP::header exists "Content-Length"] && [HTTP::method] eq "POST" }{
+	HTTP::collect [HTTP::header "Content-Length"]
     }
 }
 when HTTP_REQUEST_DATA {
-    set json_array [call LIBjson.tcl::parse_json [HTTP::payload]]
-    if { [catch {set value [call LIBjson.tcl::getxpath $json_array /0/color]} exmesg] != 0 } {
-        log local0.info "/0/color does not exist"
-        HTTP::respond 200 content "<html>/0/color does not exist</html>" Content-Type text/html
+    set path "/web-app/servlet/1/servlet-name"
+    if { [catch {set json_array [call LIBjson.tcl::parse_json [HTTP::payload]]} badparse] != 0 } {
+	log local0.err "Invalid JSON document"
+	HTTP::respond 403 content "<html>Invalid JSON document</html>" Content-Type text/html
     } else {
-        log local0.info "/0/color exists"
-        set response [format {<html>/0/color is %s</html>} $value]
-        HTTP::respond 200 content $response Content-Type text/html
+	if { [catch {set value [call LIBjson.tcl::getxpath $json_array $path]} exmesg] != 0 } {
+	    log local0.info "$path does not exist"
+	    HTTP::respond 200 content "<html>$path does not exist</html>" Content-Type text/html
+	} else {
+	    log local0.info "$path exists"
+	    set response [format {<html>%s is %s</html>} $path $value]
+	    HTTP::respond 200 content $response Content-Type text/html
+	}
     }
 }
